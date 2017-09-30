b7b60c0cdd4a4fca79e22c42cf4b3dabb9c3079c
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 15:23:32 +0100

    Finished client with reference to msdn dll code that is also a thing you made.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCodeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
         }
     }
 }
